1b13d799c7bb76ce2432e1140945d42260507be6
oliverboldt <abo2019@oliver-boldt.de>
2021-04-14 16:39:46 +0200

    first commit

---
+++ taiko-config.js
@@ -0,0 +1,11 @@
+﻿var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth -50;
+canvas.height = window.innerHeight -50;
+
+var c = canvas.getContext('2d');
+
+
+c.fillRect(100,100,200,100);
+
+console.log(canvas);
---
+++ taiko-data-json.js
@@ -0,0 +1,74 @@
+const TAIKO_DATA_JSON = 
+[
+	// handles : [ { id: "HANDLE_NONE", description: "Ohne Haltegriffe" }, { id: HANDLE_BRASS_SMALL } ]
+	// text : 
+	// [ 
+	//		{ id: "MIYA_DAIKO_14+description+de", text: "Nagado Daiko 1.4shaku"}
+	// ]
+
+	// drums : []
+	{
+		id: 			"HIRA_DAIKO_14", 
+		name: 			"Hira daiko 1,4 shaku",
+		thumbnail: 		"hira.jpg",
+		description: 	"Fell-d: 42cm",
+		description_en: "Skin-d: 42cm",
+		price: 			"750",
+		option_handle : [ "HANDLE_NONE", "HANDLE_SMALL", "HANDLE_BIG", "HANDLE_STEEL", "HANDLE_STEEL_NOBASE" ]
+	}
+	,
+	{
+		id: 			"HIRA_DAIKO_15", 
+		name: 			"Hira daiko 1,5 shaku",
+		thumbnail: 		"hira.jpg",
+		description: 	"Fell-d: 45cm",
+		price: 			"850",
+		option_handle : [ "HANDLE_NONE", "HANDLE_SMALL", "HANDLE_BIG", "HANDLE_STEEL", "HANDLE_STEEL_NOBASE" ]
+	}
+	,
+	{
+		id: 			"HIRA_DAIKO_16", 
+		name: 			"Hira daiko 1,6 shaku",
+		thumbnail: 		"hira.jpg",
+		description: 	"Fell-d: 48cm",
+		price: 			"950",
+		option_handle : [ "HANDLE_NONE", "HANDLE_SMALL", "HANDLE_BIG", "HANDLE_STEEL", "HANDLE_STEEL_NOBASE" ]
+	}
+	,
+	{
+		id: 			"HIRA_DAIKO_17", 
+		name: 			"Hira daiko 1,7 shaku",
+		thumbnail: 		"hira.jpg",
+		description: 	"Fell-d: 51cm",
+		price: 			"1050",
+		option_handle : [ "HANDLE_NONE", "HANDLE_SMALL", "HANDLE_BIG", "HANDLE_STEEL", "HANDLE_STEEL_NOBASE" ]
+	}
+	,
+	{
+		id: 			"Miya_DAIKO_14", 
+		name: 			"Miya daiko 1,4 shaku",
+		thumbnail: 		"miya-14.jpg",
+		description: 	"Fell-d: 42cm",
+		description_en: "Skin-d: 42cm",
+		price: 			"1100",
+		option_handle : [ "HANDLE_NONE", "HANDLE_SMALL", "HANDLE_BIG", "HANDLE_STEEL", "HANDLE_STEEL_NOBASE" ]
+	}
+	,
+	{
+		id: 			"MIYA_DAIKO_15", 
+		name: 			"Miya daiko 1,5 shaku",
+		thumbnail: 		"miya-15.jpg",
+		description: 	"Fell-d: 45cm Bauch-d:56cm Länge=56cm",
+		price: 			"1275",
+		option_handle : [ "HANDLE_NONE", "HANDLE_SMALL", "HANDLE_BIG", "HANDLE_STEEL", "HANDLE_STEEL_NOBASE" ]
+	}
+	,
+	{
+		id: 			"MIYA_DAIKO_16", 
+		name: 			"Miya daiko 1,6 shaku",
+		thumbnail: 		"miya-16.jpg",
+		description: 	"Fell-d: 48cm",
+		price: 			"1450",
+		option_handle : [ "HANDLE_NONE", "HANDLE_SMALL", "HANDLE_BIG", "HANDLE_STEEL", "HANDLE_STEEL_NOBASE" ]
+	}
+];
