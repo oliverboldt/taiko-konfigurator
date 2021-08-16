9e2786b7354c898bac602610e7ad32ac781de285
oliverboldt <abo2019@oliver-boldt.de>
2021-08-16 12:35:31 +0200

    English Nr. 1

--- js/loc-data-json.js
+++ js/loc-data-json.js
@@ -2,6 +2,12 @@ const LOC_DATA_JSON =
 {
 	locstrings: 
 	[
+		{
+			id: "MYTAIKODRUM",
+			language: "en",
+			text: "my<strong>taiko</strong>drum.eu"
+		}
+		,
 		{ 
 			id: "INDEX_TITLE",
 			language: "en",
@@ -17,9 +23,10 @@ const LOC_DATA_JSON =
 		{
 			id: "INDEX_PARAGRAPH_1",
 			language: "en",
-			text: "With a mighty <strong>D O N</strong> we'd like to welcome you to my<strong>taiko</strong>drum.eu ! \
-					This is the right place to starting your journey to create your own taiko drum. Step by step \
-					we'd like to help you finding a drum and customizing it to your needs."
+			text: "With a splendid <b>D O N</b> we welcome you to mytaikodrum.eu\
+					You have come to the right place to embark on your journey to finding your own personalised and individual drum.\
+					Step by step we will accompany and advise you on choosing and adapting your individual Taiko In order to match and satisfy your wishes."
+
 		}
 		,
 		{
@@ -49,10 +56,27 @@ const LOC_DATA_JSON =
 		{
 			id: "INDEX_ABOUT_MORE",
 			language: "en",
-			text: "<u>WE are the people behind mytaikodrum.eu</u>, and we love \
-				to accompany you on your way finding your perfect drum, with your rhythm \
-				and personal style."
+			text: "<u>WE are the people behind mytaikodrum.eu</u> and love to be able to advise you, \
+					taking into account your own rhythm and particular style."
 		}	
+		,
+		{
+			id: "LINK_ABOUT",
+			language: "en",
+			text: "About my<strong>taiko</strong>drum.eu"
+		}
+		,
+		{
+			id: "LINK_WORKSHOP",
+			language: "en",
+			text: "Workshop"
+		}
+		,
+		{
+			id: "LINK_BACK",
+			language: "en",
+			text: "<< back"
+		}
 		,
 		{
 			id: "IMPRESSUM",
@@ -66,29 +90,88 @@ const LOC_DATA_JSON =
 			text: "Data protection"
 		}
 		,
-		{
-			id: "MYTAIKODRUM",
+		{ 
+			id: "ABOUT_US_HEADLINE",
 			language: "en",
-			text: "my<strong>taiko</strong>drum.eu"
+			text: "About us"
+		}
+		,
+		{ 
+			id: "ABOUT_INTRO",
+			language: "en",
+			text: "Every active Taiko player wants to have his/her personal drum sooner or later. So did we:"
+		}
+		,
+		{ 
+			id: "ABOUT_ARTICLE_MARION",
+			language: "en",
+			text: "<p>I have been a passionate and intensive Taiko player since 2017. \
+				Whenever possible I visit workshops and love the contact and lively exchange with my fellow drummers.</p> \
+				<p>Something we all had in common was trying to find high quality and affordable instruments.\
+				My idea was born. Step by step and after receiving a great deal of sound advice and patience my own \
+				Taiko was built. A Taiko that suits my needs perfectly .</p>\
+				<p>I spoke to my 'Taiko pal' Oliver Boldt from Hamburg whose professional expertise,\
+				patience and understanding I really appreciate and treasure.\
+				Oliver has been building Japanese drums since the late 1990s.\
+				At first to meet his personal requirements and that of his own Taiko group.\
+				His workshop is perfectly equipped to build Taikos oriented on the original \
+				Japanese drums whilst incorporating his practical skills and experience taking the needs \
+				and wishes of other drummers into consideration. \
+				Since developing the 'Taicut.cnc'-milling machine he is able to offer really high quality \
+				Taikos at a fair price.</p>"
+		}
+		,
+		{ 
+			id: "ABOUT_ARTICLE_OLIVER",
+			language: "en",
+			text: "<p>When I began playing Taiko in 1998 there was nothing on the European market to be found.\
+				Out Of necessity a small team and I built our first Shime-daikos. Also, I got hold of a few wine \
+				barrels, converted and refined them as Nagado-daikos.\
+				I set up a small workshop and the work became easier and the quality improved.\
+				My qualification as a craftsman came to my advantage.</p>\
+				<p>Over the years I have built all of the instruments for my group 'Yo Bachi Daiko.'\
+				Shimen, Nagados, O-Daikos and Okedos. External requests also increased. \
+				Until the beginning of 2015 my main occupation was working as a software developer. \
+				This was a great help in designing the concept for the Taicut.cnc milling machine. \
+				This precision machine brings the rough form of the corpus into perfect shape, giving \
+				it our  unique sound. This was the last step to lifting Taikos built in Europe to a higher level.</p>"
+		}
+		,
+		{ 
+			id: "KONTAKT_MARION",
+			language: "en",
+			text: "Contact:"
+		}
+		,
+		{ 
+			id: "KONTAKT_OLIVER",
+			language: "en",
+			text: "Contact:"
+		}
+		,
+		{ 
+			id: "LINK_SPOT_WORKSHOP",
+			language: "en",
+			text: "Spot the workshop"
+		}
+		,
+		{ 
+			id: "id",
+			language: "en",
+			text: ""
 		}
 		,
 		
 		{ 
 			id: "HEADLINE_SIZEANDTYPE",
 			language: "en",
-			text: "Type and size of Taiko"
-		}
-		,
-		{ 
-			id: "ARTICLE_DRUMTYPE",
-			language: "en",
-			text: "Do I need a Hira- or Nagado Daiko?"
+			text: "Drum type and size"
 		}
 		,
 		{ 
 			id: "LABEL_TYPE",
 			language: "en",
-			text: "I want a "
+			text: "I would like a"
 		}
 		,
 		{ 
@@ -134,9 +217,21 @@ const LOC_DATA_JSON =
 		}
 		,
 		{ 
+			id: "OPTION_SIZE_3",
+			language: "en",
+			text: "Size 3"
+		}
+		,
+		{ 
+			id: "OPTION_SIZE_4",
+			language: "en",
+			text: "Size 4"
+		}
+		,
+		{ 
 			id: "TEXT_BASICPRICE",
 			text: "für Basispreis von",
-			text_en: "for basic price of"
+			text_en: "at the basic price of"
 		}
 		,
 		{ 
@@ -146,31 +241,60 @@ const LOC_DATA_JSON =
 		}
 		,
 		{ 
+			id: "MIYA_DAIKO",
+			language: "en",
+			text: "Nagado-daiko"
+		}
+		,
+		{ 
+			id: "ARTICLE_DRUMTYPE_HEADLINE",
+			language: "en",
+			text: "Which is the right drum for me? Nagado, Hira or Shime-daiko?"
+		}
+		,
+		{ 
+			id: "ARTICLE_DRUMTYPE",
+			language: "en",
+			text: "<p>We offer Taikos in different sizes. Our drums are produced to precisely the shape and size of the \
+				Japanese Taikos using shaku measurements. One shaku is the equivalent of 30cm. \
+				For example 1.4shaku equals a skin diameter of 42cm (1.5=45cm; 1.6=48cm). \
+				Shime-daikos come in sizes  1 to 5 in Japan.</p>"
+		}
+		,
+		{ 
 			id: "ARTICLE_DRUMTYPE_MIYA",
 			language: "en",
-			text: "<p>We offer Nagado-daikos and Hira-daikos in different sizes. Our Taikos are precisely manufactured in shape \
-				and size as specified in Japan and in the unit of measurement Shaku. A shaku is almost exactly 30cm. \
-				Thus, 1.4 shaku corresponds to a playable skin diameter of 42 cm (42 Nagado). 1.6 shaku are accordingly \
-				48cm (playable) skin diameter (48er Nagado).</p>\
-				<p>A <b>48 Nagado-daiko (1,6 shaku)</b> is a very universal drum. You can play them with very thick bachi, which is \
-				very interesting for the side punch technique (Japanese &quot;Yoko-uchi&quot; or &quot;Naname-uchi&quot;). \
-				Due to its size and weight of approx. 20kg, this drum is of course more complex to transport. The height \
-				 of the Nagado-daiko is 60cm.</p> \
-				<p>A <b>42 Nagado-daiko (1,4 shaku)</b> has clear advantages here and is in no way inferior to its larger sister in terms \
-				of looks and sound. It weighs less than 14kg and fits in every car trunk and also likes to go on the back seat. \
-				It also cuts a fine figure on an inclined stand and can still be played powerfully with medium Bachi. The height is 52.5 cm.</p>"
+			text: "<p><b>A 48cm Nagado-daiko (1.6shaku)</b> is a universal drum. \
+				You can play it with very thick bachi which is really interesting for the lateral strike technique \
+				(Japanese “Yoko-uchi” or “Naname-uchi“). This drum is of course more difficult to transport because \
+				of its size and weight, approximately 20kg and at a height of 60cm. </p>\
+				<p><b>A 42cm Nagado-daiko (1.4shaku)</b> has a definite advantage. It  looks and sounds just like its \
+				bigger sister  weighing only about 14kg and with a height of 52,5cm it fits into every car boot or on the rear seat. \
+				It also looks good on the sland stand and can be energetically played with medium sized bachi. </p>"
 		}
 		,
 		{ 
 			id: "ARTICLE_DRUMTYPE_HIRA",
 			language: "en",
-			text: "<p>If you want your Taiko to be even more mobile, you like to travel by bus and train and you want your Taiko to go with \
-			every training or excursion, a <b>Hira-daiko (Japanese for &quot;flat drum&quot;)</b> suits you. We build them in 4 sizes as 42 mm \
-			(playable skin diameter 42 cm) to 51 cm (1.4 to 1.7 shaku). The height of the drum body is about half that of a Miya-daiko. \
-			Such a drum is much lighter and can be transported without a car. The sound of a hira-daiko is also flatter and the tone longer \
-			than that of a miya. For the Taiko styles Yoko-uchi (Japanese for &quot;hit sideways&quot;) and Naname-uchi (Japanese for \
-				&quot;hit at an angle&quot;) you need a Miya-daiko described above.</p>"
-
+			text: "<p>If you are travelling by public transport and you want to take your Taiko to training \
+				sessions or on outings, a <b>Hira-daiko (Japanese flat drum)</b> is right for you. \
+				We build them in 4 sizes from 42cm (skin diameter) to 51cm (1.4 to 1.7 shaku) \
+				The drum corpus is approximately half the height of a Nagado-daiko making it much \
+				lighter and easier to transport. \
+				The sound is flatter and the tone longer than a Nagado. </p>"
+		}
+		,
+		{ 
+			id: "ARTICLE_DRUMTYPE_SHIME",
+			language: "en",
+			text: "<p>No group should be without a laced Shime-daiko.We have been building them since the year 2000 \
+				and the first instruments are still in use. \
+				Only the best parts of the skin are used and they make the typical crisp sound. \
+				The rope model weighs about 6 kg. \
+				We will be happy to show you how to readjust the tension without needing any technical aids. \
+				<b>Size 3 is a real classic and is always a first choice.</b> \
+				The corpus of size 4 is at 21 cm about 3 cm higher than the size 3 and uses thicker skin. \
+				With relevant effort it is possible to adjust the tension to a higher degree.</p>"
 		}
 	
 	],
@@ -206,6 +330,21 @@ const LOC_DATA_JSON =
 	localizePage : function()
 	{
 		var elements = document.querySelectorAll(".localize");
+		var lang = localStorage.getItem("lang");
+
+		for (var i=0; i<elements.length; i++)
+		{
+			var locText = this.getLocstring(elements[i].id,lang);
+
+			if (locText != null)
+				elements[i].innerHTML = locText;
+		}
+	}
+	,
+	localizeElement : function(name)
+	{
+		var element = document.querySelector("#" + name);
+		var elements = element.querySelectorAll(".localize");
 		var lang = localStorage.getItem("lang");
 
 		for (var i=0; i<elements.length; i++)
